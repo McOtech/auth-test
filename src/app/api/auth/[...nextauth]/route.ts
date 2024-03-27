1ae4715dc1698b9bfd025a49901ed25e2e51f2d4
McOtech <otienopaul36@gmail.com>
2024-03-27 10:34:14 +0300

    refactor: console logs

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -9,31 +9,31 @@ const handler = NextAuth({
       issuer: process.env.COGNITO_ISSUER,
     }),
   ],
-  secret: "qawsexrtyuihjiokiop",
+  secret: process.env.SECRETE,
   //   session: {
   //     strategy: "jwt",
   //   },
   callbacks: {
     async session({ session, user, token }) {
-      //   console.log("session::token:::>", token);
-      //   console.log("session::session:::>", session);
-      //   console.log("session::user:::>", user);
+        console.log("session::token:::>", token);
+        console.log("session::session:::>", session);
+        console.log("session::user:::>", user);
 
-      //   if (session.user) {
-      //     session.user = { ...session.user, name: token.username as string };
-      //     return session;
-      //   }
+        if (session.user) {
+          session.user = { ...session.user, name: token.username as string };
+          return session;
+        }
       return session;
     },
     async jwt({ token, user, account, profile, isNewUser }) {
-      //   console.log("jwt::account:::>", account);
-      //   console.log("jwt::profile:::>", profile);
-      //   console.log("jwt::token:::>", token);
-      //   if (profile) {
-      //     const _profile: any = profile;
-      //     token.username = _profile["cognito:username"];
-      //     return token;
-      //   }
+        console.log("jwt::account:::>", account);
+        console.log("jwt::profile:::>", profile);
+        console.log("jwt::token:::>", token);
+        if (profile) {
+          const _profile: any = profile;
+          token.username = _profile["cognito:username"];
+          return token;
+        }
       return token;
     },
   },
